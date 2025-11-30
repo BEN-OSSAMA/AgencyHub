1b31ff9d4a6cf4ed87e835b9b6fcc0a5ee169d1e
BEN-OUSSAMA <ossama.sm17@gmail.com>
2025-11-30 01:48:24 +0100

    ossama commit

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: [
-    "/", 
-    "/sign-in",
-    "/sign-up",
-  ],
+  publicRoutes: ["/", "/sign-in", "/sign-up"],
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|static|favicon.ico|.*\\..*).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!_next|.*\\..*).*)", "/(api|trpc)(.*)"],
 };
